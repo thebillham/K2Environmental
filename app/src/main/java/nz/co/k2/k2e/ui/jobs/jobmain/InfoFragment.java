9d2f75226212150f4967dfc6f2ce6bd8aaa53553
Ben D <thebillham@gmail.com>
2018-07-17 22:06:05 +1200

    Using fotoapparat for camera - still not able to save images!

--- app/src/main/java/nz/co/k2/k2e/ui/jobs/jobmain/InfoFragment.java
+++ app/src/main/java/nz/co/k2/k2e/ui/jobs/jobmain/InfoFragment.java
@@ -14,18 +14,26 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import nz.co.k2.k2e.utils.CameraUtils;
-import nz.co.k2.k2e.utils.ImageUtils;
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.log.LoggersKt;
+import io.fotoapparat.result.PhotoResult;
+import io.fotoapparat.selector.ResolutionSelectorsKt;
+import io.fotoapparat.view.CameraView;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 import nz.co.k2.k2e.BR;
 import nz.co.k2.k2e.R;
 import nz.co.k2.k2e.databinding.FragmentJobmainInfoBinding;
 import nz.co.k2.k2e.ui.base.BaseFragment;
+import nz.co.k2.k2e.utils.CameraUtils;
+import nz.co.k2.k2e.utils.ImageUtils;
 
 public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobViewModel> {
 
@@ -39,13 +47,16 @@ public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobVi
     @Inject
     JobViewModel mJobViewModel;
 
+    Fotoapparat fotoapparat;
+
     private String title;
     private int page;
+    private Boolean isCamera = false;
 
     // Image view for showing our image.
     private ImageView mImageView;
 
-    private static final int PICK_IMAGE_ID = 1111;
+//    private static final int PICK_IMAGE_ID = 1111;
 
     @Override
     public int getBindingVariable() {
@@ -64,8 +75,15 @@ public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobVi
     }
 
     @Override
+    public void onResume(){
+        super.onResume();
+        fotoapparat.start();
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
+        fotoapparat.stop();
         getCompositeDisposable().add(mJobViewModel.getDataManager().updateJob(mJobViewModel.currentJob.get())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -78,13 +96,44 @@ public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobVi
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = super.onCreateView(inflater, container, savedInstanceState);
-        mImageView = view.findViewById(R.id.image_title_photo);
+        CameraView cameraView = view.findViewById(R.id.image_title_camera);
+        ImageView imageView = view.findViewById(R.id.image_title_photo);
         ImageButton jobTitlePhotoBtn = view.findViewById(R.id.btn_title_photo);
+
+        fotoapparat = Fotoapparat
+                .with(getActivity())
+                .into(cameraView)
+                .photoResolution(ResolutionSelectorsKt.highestResolution())
+                .logger(LoggersKt.fileLogger(getContext()))
+                .build();
 
         jobTitlePhotoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(CameraUtils.getPickImageIntent(getActivity()), PICK_IMAGE_ID);
+                    if (isCamera){
+                        try {
+                            PhotoResult photoResult = fotoapparat.takePicture();
+                            photoResult.saveToFile(ImageUtils.createImageFile(getActivity()));
+                            photoResult.toBitmap()
+                            .whenAvailable(bitmapPhoto -> {
+                                imageView.setImageBitmap(bitmapPhoto.bitmap);
+                                imageView.setRotation(-bitmapPhoto.rotationDegrees);
+                                return Unit.INSTANCE;
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        fotoapparat.stop();
+                        cameraView.setVisibility(View.GONE);
+                        imageView.setVisibility(View.VISIBLE);
+                        isCamera = false;
+                    } else {
+                        fotoapparat.start();
+                        cameraView.setVisibility(View.VISIBLE);
+                        imageView.setVisibility(View.GONE);
+                        isCamera = true;
+                    }
+//                startActivityForResult(CameraUtils.getPickImageIntent(getActivity()), PICK_IMAGE_ID);
             }
         });
         return view;
@@ -104,8 +153,9 @@ public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobVi
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt", 0);
-        title = getArguments().getString("someTitle");
+//        page = getArguments().getInt("someInt", 0);
+//        title = getArguments().getString("someTitle");
+
     }
 
     @Override
@@ -113,17 +163,18 @@ public class InfoFragment extends BaseFragment<FragmentJobmainInfoBinding, JobVi
         super.onViewCreated(view, savedInstanceState);
         fragmentJobmainInfoBinding = getViewDataBinding();
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        switch(requestCode){
-            case PICK_IMAGE_ID:
-                Bitmap bitmap = CameraUtils.getImageFromResult(getActivity(), resultCode, data);
-                mImageView.setImageBitmap(bitmap);
-                break;
-            default:
-                super.onActivityResult(requestCode,resultCode,data);
-                break;
-        }
-    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data){
+//        switch(requestCode){
+//            case PICK_IMAGE_ID:
+//                Bitmap bitmap = null;
+//                bitmap = CameraUtils.getImageFromResult(getActivity(), resultCode, data);
+//                mImageView.setImageBitmap(bitmap);
+//                break;
+//            default:
+//                super.onActivityResult(requestCode,resultCode,data);
+//                break;
+//        }
+//    }
 }
